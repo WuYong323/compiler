b55b7e4396d3523d4b74f05844687f8bbfc718de
余锶埼 <177228770@qq.com>
2026-09-19 22:29:17 +0800

    update

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ set "SRC="
 for %%f in (src\*.cpp) do set "SRC=!SRC! %%f"
 g++ -std=c++17 -Wall -Wextra -O2 -o coolc.exe %SRC%
 if errorlevel 1 (echo BUILD FAILED & exit /b 1)
-echo BUILD OK -> coolc.exe
+echo BUILD OK -^> coolc.exe
